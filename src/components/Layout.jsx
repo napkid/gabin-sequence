9ec4e534595d32d053215cebf3ca7d5b599966d1
Clément Dandrieux <clement.dandrieux@gmail.com>
2017-06-04 12:19:32 +0200

    Finished design

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,30 +9,35 @@ export default class Layout extends React.Component {
   render(){
     return(
       <div id='app-layout'>
-        <div className="jumbotron">
-          <h1 className="display-3">The Gabin Sequence</h1>
-          <p className="lead">A little math experiment</p>
-          <hr />
-          <p>This little math experiment app lets you input a few number & run the calculation of the suite.<br />
-          My friend Gabin asked me to code something to test the suite he imagined, so that's it !</p>
-          <p className="lead">
-            <a className="btn btn-primary btn-lg" href="#try" role="button">I wanna try that !</a>
-          </p>
-        </div>
-        <div style={{height:"1vh"}}>
-          <div className="container-fluid" id="try">
-            {this.props.children}
-          </div>
-          <div className="card text-center" style={{marginTop:"3em"}}>
-            <div className="card-block">
-              <p class="card-text">This project is released under the <a href="https://github.com/napkid/gabin-sequence/blob/master/LICENSE">MIT License</a>.
-              <br />Special thanks to Gabin Perez !
-              <br />(c) Copyright Clément Dandrieux 2017
-              </p>
+        <div className="card" style={{color:"white",backgroundColor:"#2188b6"}}>
+          <div className="card-block">
+            <div className="text-center">
+              <h1 className="display-3 font-weight-bold">The Gabin Sequence</h1>
+              <p className="lead">A little math experiment</p>
             </div>
+
+            <hr style={{margin:"3em"}}/>
+            <p>This little math experiment app lets you input a few number & run the calculation of the suite.<br />
+            My friend Gabin asked me to code something to test the suite he imagined, so that's it !</p>
+            <p className="lead">
+              <a className="btn btn-outline-success btn-lg" href="#try" role="button" style={{border:"0.1rem solid white", color:"white"}}>I wanna try that !</a>
+            </p>
+        </div>
+      </div>
+      <div style={{height:"1vh"}}>
+        <div className="container-fluid" id="try">
+          {this.props.children}
+        </div>
+        <div className="card text-center" style={{marginTop:"3em"}}>
+          <div className="card-block">
+            <p class="card-text">This project is released under the <a href="https://github.com/napkid/gabin-sequence/blob/master/LICENSE">MIT License</a>.
+            <br />Special thanks to <strong>Gabin Perez</strong> !
+            <br /><strong>(c) Copyright Clément Dandrieux 2017</strong>
+            </p>
           </div>
         </div>
       </div>
+    </div>
     )
   }
 }
